65c6d14011fb25a2c894ccd0960fff586b0ad9cd
Tom Pierce <tompierce4@gmail.com>
2023-09-22 12:57:02 +0100

    update createPlayer tests to work with player.opponent

--- src/factories/__tests__/createPlayer.test.js
+++ src/factories/__tests__/createPlayer.test.js
@@ -1,15 +1,13 @@
 import createPlayer from "../createPlayer";
 
 const player = createPlayer("Jeremy");
-const opponent = createPlayer("Jeffrey");
-
-test("Create player", () => {
-  expect(player.name).toBe("Jeremy");
-});
+const computer = createPlayer("Jeffrey");
+player.opponent = computer;
+computer.opponent = player;
 
 test("Player attack", () => {
-  opponent.gameboard.placeShip(0, 0, 1, true);
-  expect(opponent.gameboard.gameOver).toBe(false);
-  player.takeShot(0, 0, opponent);
-  expect(opponent.gameboard.gameOver).toBe(true);
+  computer.gameboard.placeShip(0, 0, 1, true);
+  expect(computer.gameboard.gameOver).toBe(false);
+  player.takeShot(0, 0, computer);
+  expect(computer.gameboard.gameOver).toBe(true);
 });
